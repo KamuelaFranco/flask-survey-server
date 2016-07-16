4cc8fad215c64d5737542b73505a2ecc601f9e08
Kamuela Franco <kamuela.franco@gmail.com>
2016-07-16 18:55:55 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = ''
+
+db = SQLAlchemy(app)
+
+@app.route('/admin')
+def index():
+    return 'Admin endpoint for displaying results'
+
+@app.route('/survey')
+def create():
+    return 'Survey endpoint for POSTing survey'
+
+if __name__ == '__main__':
+    app.run(debug=False)
