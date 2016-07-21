314b85ed1d3efc3ee8049a056801fd1b56cf682a
Kamuela Franco <kamuela.franco@gmail.com>
2016-07-21 17:26:53 +0100

    Added UUID and completion flag

--- server.py
+++ server.py
@@ -2,44 +2,48 @@ from flask_sqlalchemy import SQLAlchemy
 
 import flask
 import flask_cors
+import json
+import os
 import sys
 
 # Server config
-
 app = flask.Flask(__name__)
-
 flask_cors.CORS(app)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = \
-    'postgres://wwanvaxpkgkakx:GZR1rVHGdyL3t_M8LW_VYQfpHP@ec2-54-83-44-229.compute-1.amazonaws.com:5432/deibu97vueiugu'
+try:
+    DATABASE_URL = os.environ['DATABASE_URL']
+except KeyError:
+    sys.exit('Error: Provide DATABASE_URL environment key')
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db = SQLAlchemy(app)
 
 # Database models
 ## Name, Email, Age, About me (block), Address, Gender (male/female)
 ## Favourite book, Favourite Colours (multiple)
-
 class SurveyResult(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    email = db.Column(db.String(100))
+    name = db.Column(db.String)
+    email = db.Column(db.String)
     age = db.Column(db.Integer)
     about_me = db.Column(db.Text)
     address = db.Column(db.Text)
-    favourite_book = db.Column(db.String(100))
-    favourite_colours = db.Column(db.String(100))
+    favourite_book = db.Column(db.String)
+    favourite_colours = db.Column(db.String)
     gender = db.Column(db.Integer)
+    uuid = db.Column(db.String(36))
+    is_complete = db.Column(db.Boolean)
 
     def __init__(self, query_params):
-        self.name = query_params.get('name', '')
-        self.email = query_params.get('email', '')
+        self.name = query_params.get('name')
+        self.email = query_params.get('email')
         self.age = int(query_params.get('age', 0))
-        self.about_me = query_params.get('about_me', '')
-        self.address = query_params.get('address', '')
+        self.about_me = query_params.get('about_me')
+        self.address = query_params.get('address')
         self.gender = int(query_params.get('gender', 0))
-        self.favourite_book = query_params.get('favourite_book', '')
-        self.favourite_colours = query_params.get('favourite_colours', '')
+        self.favourite_book = query_params.get('favourite_book')
+        self.favourite_colours = query_params.get('favourite_colours')
+        self.uuid = query_params.get('uuid')
+        self.is_complete = json.loads(query_params.get('is_complete', False))
 
     def __repr__(self):
         return '<User %r>' % self.name
@@ -54,13 +58,14 @@ class SurveyResult(db.Model):
             'gender': self.gender,
             'favourite_book': self.favourite_book,
             'favourite_colors': self.favourite_colours,
+            'is_complete': self.is_complete
         }
 
 # Routes
 
 @app.route('/')
 def index():
-    return 'Endpoints: /admin, /survey'
+    return flask.json.jsonify(success='Server running')
 
 @app.route('/admin')
 def show():
@@ -68,24 +73,32 @@ def show():
         survey_results = SurveyResult.query.all()
         return flask.json.jsonify(success=True, results=[r.serialise() for r in survey_results])
     except:
-        error_message = 'Could not read from database'
         print str(sys.exc_info())
-        return flask.json.jsonify(error=error_message)
+        return flask.json.jsonify(error='Could not read from database')
 
-@app.route('/survey')
-def create():
+@app.route('/survey', methods=['GET'])
+def update():
     query_params = flask.request.args.to_dict()
-    try:
-        survey_result = SurveyResult(query_params=query_params)
-        db.session.add(survey_result)
-        db.session.commit()
-        return flask.json.jsonify(success=True, params=query_params)
-    except:
-        error_message = 'Could not write to database'
-        print str(sys.exc_info())
-        return flask.json.jsonify(error=error_message)
+    uuid = query_params.get('uuid')
+    if uuid and len(uuid) == 36:
+        prev_survey_result = SurveyResult.query.filter_by(uuid=uuid).first()
+        if prev_survey_result is None:
+            # Create new record
+            try:
+                survey_result = SurveyResult(query_params=query_params)
+                db.session.add(survey_result)
+                db.session.commit()
+                return flask.json.jsonify(success=True, params=query_params)
+            except:
+                print str(sys.exc_info())
+                return flask.json.jsonify(error='Could not write to database')
+        elif prev_survey_result.is_complete:
+            return flask.json.jsonify(error='Survey has already been completed')
+        else:
+            # Update record
+            return flask.json.jsonify(error='Updating a record has not been implemented yet')
+    return flask.json.jsonify(error='Invalid UUID or UUID not provided')
 
 # Start server
-
 if __name__ == '__main__':
     app.run()
